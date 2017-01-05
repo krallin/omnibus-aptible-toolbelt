e781be69e2094ad6a35aceec23d567b1b87d9d86
Thomas Orozco <thomas@orozco.fr>
2017-01-05 11:35:00 +0100

    Support disabling clean fetches on builds
    This makes it easier to iterate.

--- util/clean_software.rb
+++ util/clean_software.rb
@@ -7,10 +7,14 @@
 # This fixes the issue by always cleaning software sources, regardless of
 # whether they'll be built or restored from cache (which also means we clean
 # them twice when actually building... but that's the lesser of two evils).
+#
+# This patch can be disabled using OMNIBUS_SKIP_CLEAN, which allows for faster
+# iteration speed in development (it avoids having to re-extract every single
+# source).
 
 module CleanSoftwareExtension
   def build_me(*args)
-    #fetcher.clean
+    fetcher.clean unless ENV['OMNIBUS_SKIP_CLEAN']
     super(*args)
   end
 end
